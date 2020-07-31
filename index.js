cf11168af254cf2bd0ff5ddd3cfc886c67a8be5e
jahallb1 <62314714+jahallb1@users.noreply.github.com>
2020-07-31 19:41:29 -0400

    added new stuff to it

---
+++ index.js
@@ -0,0 +1,68 @@
+// array of questions for user
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const questions = [ 
+    {
+    type: 'input',
+    name: 'name',
+    message: 'What is the name of your project? (Required)',
+    validate: nameInput => {
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Please enter the name of your project!');
+            return false;
+        }
+    }
+},
+//rest of questions
+{
+    type: 'input',
+    name: 'description',
+    message: 'Give a description of your project'
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Give installation instructions for your project'
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Give usage instructions for your project'
+},
+{
+    type: 'input',
+    name: 'contribution',
+    message: 'Give contribution instructions for your project'
+},
+{
+    type: 'input',
+    name: 'tests',
+    message: 'Please give any tests for your project'
+},
+{
+    type: 'checkbox',
+    name: 'license',
+    message: 'Which license would you like to use?',
+    choices: ['MIT', 'BSD', 'UNKNOWN']
+}
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+    inquirer.prompt(questions)
+        .then ((responses) => {
+            writeToFile();
+        })
+
+}
+
+// function call to initialize program
+init();
